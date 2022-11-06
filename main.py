c4f9fd0ff69ecafac3c144d63891419a3f635540
Raphael Fachim <raphaelsiller@gmail.com>
2022-11-06 12:12:17 -0300

    calculando e mostrando a fft de uma senoide com numpy

--- main.py
+++ main.py
@@ -1,9 +1,32 @@
 import src.controller.dados.importardados as importar
+import src.controller.frequencia.fft as fft
+import numpy as np
+import matplotlib.pyplot as plt
 from src.model.modelos.variaveissensor import VariaveisSensor
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    a = VariaveisSensor("ai2", "ai3", "ai4")
+    acel = VariaveisSensor("ai2", "ai3", "ai4")
     caminho = "C:\\Users\\rafas\\Desktop\\frequency-analysis\\src\\model\\data\\Dev4_1.txt"
     nome_raiz = "Dev4"
 
-    b = importar.ler_dados_acelerometro(caminho, nome_raiz, a)
+    acel = importar.ler_dados_acelerometro(caminho, nome_raiz, acel)
+
+    # f = 20 hz
+    # N = 1000 samples/s
+    # t = 5 s
+    f = 20
+    N = 1000
+    Lx = 1000*5
+    omega = 2.0*np.pi*f
+
+    t = np.linspace(0, 5, Lx)
+
+    x = 5*np.sin(omega*t)
+
+    f, espectro = fft.fft(x, quantidade_amostras=Lx, tempo_amostragem=1/N)
+
+    plt.figure(1)
+    plt.plot(f, espectro)
+    plt.title("FFT")
+
+    plt.show()
--- src/controller/frequencia/fft.py
+++ src/controller/frequencia/fft.py
@@ -1,2 +1,11 @@
-def fft(X):
-    print("calculando fft");
+import numpy as np
+from typing import List
+
+
+def fft(sinal: List[float], quantidade_amostras: int, tempo_amostragem: float):
+    frequencias = np.fft.fftfreq(quantidade_amostras, tempo_amostragem)
+    mod = 2*np.abs(np.fft.fft(sinal)/quantidade_amostras)
+
+    mascara = frequencias > 0
+
+    return frequencias[mascara], mod[mascara]
